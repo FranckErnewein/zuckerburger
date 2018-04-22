b871f2143d1d3bbc904f963537a6ad0e379066bf
Franck Ernewein <franck.ernewein@gmail.com>
2018-04-22 17:24:27 +0200

    first try with trackingjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import testImg from './img/test.jpg';
+import './App.css';
 
 class App extends Component {
+  state = {
+    rect: []
+  };
+  componentDidMount() {
+    const tracker = new window.tracking.ObjectTracker(['face']);
+    tracker.setInitialScale(4);
+    tracker.setStepSize(2);
+    tracker.setEdgesDensity(0.1);
+
+    if (this.img) {
+      this.img.onload = () => {
+        window.tracking.track('#img-to-track', tracker);
+        tracker.on('track', event => {
+          event.data.forEach(rect => {
+            this.setState({
+              rect: [...this.state.rect, rect]
+            });
+          });
+        });
+      };
+    }
+  }
   render() {
-    return <div className="App">Zuckerburger</div>;
+    const { rect } = this.state;
+    return (
+      <div className="App">
+        <img
+          ref={img => (this.img = img)}
+          id="img-to-track"
+          src={testImg}
+          alt="test"
+        />
+        {rect.map((track, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                width: track.width,
+                height: track.height,
+                top: track.y,
+                left: track.x,
+                position: 'absolute',
+                border: '1px solid red'
+              }}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
